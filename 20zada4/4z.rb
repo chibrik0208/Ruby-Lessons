cd2abe251cd56b42293045e5d22c3a0facb30c67
chibrik0208 <ilya.ismailau@gmail.com>
2024-04-01 21:07:33 +0200

    4zada4a

---
+++ 20zada4/4z.rb
@@ -0,0 +1,15 @@
+arr = ['amogus','dota','css','needforspeed']
+arr2 = []
+i = 0
+b = nil
+for element in arr
+    if element.length > i    
+    i += element.length
+    b = element    
+    end     
+end
+puts b
+
+
+
+
