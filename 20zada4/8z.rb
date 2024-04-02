4f1c785be68554b97505fc996e05f5644a18ac13
chibrik0208 <ilya.ismailau@gmail.com>
2024-04-02 22:11:24 +0200

    8zada4a

---
+++ 20zada4/8z.rb
@@ -0,0 +1,9 @@
+arr = ['мне',22.3,'года','а','моя','машина','на',3,'года','старше','меня']
+i = []
+for element in arr
+    if element.is_a?(Integer) || element.is_a?(Float) 
+    i << element
+    end
+end
+p i      
+
