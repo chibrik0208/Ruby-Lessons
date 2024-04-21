bab13dd79b57770166f9f125aafc35f1a86d9835
chibrik0208 <ilya.ismailau@gmail.com>
2024-04-21 18:19:22 +0200

    LessonClass2

---
+++ CLASS/CLASS2/main.rb
@@ -0,0 +1,21 @@
+require './human.rb'
+
+dima = Human.new('Дима', 'Кисунин')
+bogdan = Human.new('Богдан', 'Тильтович')
+ilya = Human.new('Илья', 'Цивикович')
+
+puts 'У нас есть 3 человека'
+
+sleep 2
+
+puts dima.human
+
+sleep 1
+
+puts bogdan.human
+
+sleep 1
+
+puts ilya.human
+
+
