f45099eecdc5c05798fdc42b4a723834fe6e7bf6
chibrik0208 <ilya.ismailau@gmail.com>
2024-04-21 18:18:37 +0200

    LessonClass2

---
+++ CLASS/CLASS2/human.rb
@@ -0,0 +1,12 @@
+class Human
+    
+    def initialize(name, last_name)
+    @name = name
+    @last_name = last_name
+    end
+
+    def human
+    return "#{@name} #{@last_name}"
+    end
+
+end     
