6efcdb6a70719ae4784d87254b64c9818a88c6d7
chibrik0208 <ilya.ismailau@gmail.com>
2024-05-31 21:51:18 +0200

    solution22

---
+++ CODEWARS7KATA/22.rb
@@ -0,0 +1,12 @@
+# Usually when you buy something, 
+# you're asked whether your credit card number, phone number or answer to your most secret question is still correct.
+#  However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+# Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+
+def maskify(cc)
+  
+  cc.chars.map.with_index { |element,index| index < cc.length - 4  ? '#' : element}.join
+ 
+end
