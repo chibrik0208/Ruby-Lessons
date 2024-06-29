cec16311eb8f5888032bc429e69e64158670d375
chibrik0208 <ilya.ismailau@gmail.com>
2024-06-29 14:37:17 +0200

    solution31

---
+++ CODEWARS7KATA/31.rb
@@ -0,0 +1,25 @@
+# The museum of incredibly dull things
+# The museum of incredibly dull things wants to get rid of some exhibits.
+#  Miriam, the interior architect, comes up with a plan to remove the most boring exhibits.
+#   She gives them a rating, and then removes the one with the lowest rating.
+
+# However, just as she finished rating all exhibits,
+#  she's off to an important fair, so she asks you to write a program that tells her the ratings of the exhibits after removing the lowest one.
+#   Fair enough.
+
+# Task
+# Given an array of integers, remove the smallest value. 
+# Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with the lowest index. 
+# If you get an empty array/list, return an empty array/list.
+
+# Don't change the order of the elements that are left.
+
+def remove_smallest(numbers)
+  if numbers.uniq.length == numbers.length
+  c = numbers.delete(numbers.min)
+  numbers
+  else
+  c = numbers.index(numbers.min)
+  numbers.select.with_index { |x,index| index != c ? x : nil}
+  end  
+end
