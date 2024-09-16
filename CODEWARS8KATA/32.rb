c394e1cc0ba1fba740c11e3a81e4c89da69fc95f
chibrik0208 <ilya.ismailau@gmail.com>
2024-09-16 20:03:46 +0200

    solution32

---
+++ CODEWARS8KATA/32.rb
@@ -0,0 +1,6 @@
+# Create a function named combineNames/combine_names/CombineNames that accepts two parameters 
+# (first and last name). The function should return the full name.
+
+def combine_names(name,surname)
+  return "#{name} #{surname}"
+end 
