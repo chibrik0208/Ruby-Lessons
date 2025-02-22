571a00098f850f194d8c49e8b92b468a0255bb65
chibrik0208 <ilya.ismailau@gmail.com>
2025-02-22 15:16:51 +0100

    weather update

--- weather2.rb
+++ weather2.rb
@@ -5,14 +5,14 @@ pora = ARGV[1]
 if pogoda == nil && pora == nil  
     puts 'vvedite pogodu'
     pogoda = STDIN.gets.chomp.to_i
-    puts 'daj poru'
+    puts 'daj poru 0 - vesna, 1 - leto, 2 - osien, 3 - zima'
     pora = STDIN.gets.chomp.to_i
 end    
 
 
-if pogoda.to_i >= 15 && pogoda.to_i <=35 && pora.to_i == 1 
+if pogoda.to_i >= 15 && pogoda.to_i <= 35 && pora.to_i == 1 
     puts 'Соловьи поют!'
-elsif pogoda.to_i < 22 || pogoda.to_i > 30 && pora.to_i != 1
+elsif pogoda.to_i < 22 || pogoda.to_i > 30 
     puts 'Соловьи не поют'
 else
     puts 'Cоловьи поют!'
