8fb8583cc8a4113e87cc0dccb16fee916c994ceb
chibrik0208 <ilya.ismailau@gmail.com>
2025-03-29 13:55:11 +0100

    my minigame

---
+++ OOP/minigame/warrior.rb
@@ -0,0 +1,8 @@
+class Warrior < Character
+  attr_accessor :name, :health, :damage, :defence
+
+  def damage_taking(hero,hero1,hero2)
+    hero.health = hero.health - (hero1.damage + hero2.damage) * defence  
+  end
+
+end
