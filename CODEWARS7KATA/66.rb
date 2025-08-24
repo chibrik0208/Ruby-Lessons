35ad8c9489d5c2359f8d3f7a2b50072beaf53f14
chibrik0208 <ilya.ismailau@gmail.com>
2025-08-24 14:48:10 +0200

    solution66

---
+++ CODEWARS7KATA/66.rb
@@ -0,0 +1,29 @@
+# In this Kata, your function receives an array of integers as input. Your task is to determine whether the numbers are in ascending order.
+#  An array is said to be in ascending order if there are no two adjacent integers where the left integer exceeds the right integer in value.
+
+# For the purposes of this Kata, you may assume that all inputs are valid, i.e. arrays containing only integers.
+
+# Note that an array of 0 or 1 integer(s) is automatically considered to be sorted
+#  in ascending order since all (zero) adjacent pairs of integers satisfy the condition that the left integer does not exceed the right integer in value.
+
+def is_asc_order(a)
+  a.sort == a 
+end
+
+def is_asc_order(a)
+  int = a.first
+  arr = []
+  a.each do |el|
+   if el >= int
+     arr << true
+   else
+     arr << false
+   end
+   int = el  
+  end
+  if arr.include?(false)
+    false
+  else
+    true
+  end
+end  
