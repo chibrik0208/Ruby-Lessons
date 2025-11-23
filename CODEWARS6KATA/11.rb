48f786abfd02d8aa8442d6dc0da317175e3b3604
chibrik0208 <ilya.ismailau@gmail.com>
2025-11-23 16:26:55 +0100

    solution 11

---
+++ CODEWARS6KATA/11.rb
@@ -0,0 +1,11 @@
+# Digital root is the recursive sum of all the digits in a number.
+# Given n, take the sum of the digits of n. If that value has more than one digit, 
+# continue reducing in this way until a single-digit number is produced. 
+# The input will be a non-negative integer.
+
+def digital_root(n)
+  while n.digits.length >= 2
+    n = n.digits.sum
+  end
+  n
+end
